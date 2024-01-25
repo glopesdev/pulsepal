014f489451933914082058c4c678df032ca07c7d
RoboDoig <erskine94@gmail.com>
2024-01-25 10:31:00 -0500

    Add debug client ID operator

---
+++ Bonsai.PulsePal/SetClientId.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.PulsePal
+{
+    public class SetClientId : Sink<string>
+    {
+        [TypeConverter(typeof(PortNameConverter))]
+        [Description("The name of the serial port used to communicate with the Pulse Pal.")]
+        public string PortName { get; set; }
+
+        public override IObservable<string> Process(IObservable<string> source)
+        {
+            return Observable.Using(
+                () => PulsePalManager.ReserveConnection(PortName),
+                pulsePal => source.Do(val => {
+                    lock (pulsePal.PulsePal)
+                    {
+                        pulsePal.PulsePal.SetClientId(val);
+                    }
+                })
+            );
+        }
+    }
+}
