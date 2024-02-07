8eea2ab0600bcd4dc9b6e4bc137be66d29a3fb4c
glopesdev <glopesdev@users.noreply.github.com>
2024-02-07 09:26:54 +0000

    Remove legacy unused channel configuration classes

--- Bonsai.PulsePal/CreatePulsePal.cs
+++ Bonsai.PulsePal/CreatePulsePal.cs
@@ -3,37 +3,50 @@ using System.ComponentModel;
 using System.Linq;
 using System.Reactive.Linq;
 using Bonsai.IO.Ports;
+using Bonsai.PulsePal.Configuration;
 
 namespace Bonsai.PulsePal
 {
-    [Description("Creates and configures a serial connection to a Pulse Pal device.")]
+    /// <summary>
+    /// Represents an operator that creates and configures a Pulse Pal device.
+    /// </summary>
+    [DefaultProperty(nameof(ChannelParameters))]
+    [Description("Creates and configures a Pulse Pal device.")]
     public class CreatePulsePal : Source<PulsePal>, INamedElement
     {
-        readonly PulsePalConfiguration configuration = new PulsePalConfiguration();
+        readonly PulsePalConfiguration configuration = new();
 
+        /// <summary>
+        /// Gets or sets the optional alias for the Pulse Pal device.
+        /// </summary>
         [Description("The optional alias for the Pulse Pal device.")]
         public string Name { get; set; }
 
+        /// <summary>
+        /// Gets or sets the name of the serial port used to communicate with the
+        /// Pulse Pal device.
+        /// </summary>
         [TypeConverter(typeof(SerialPortNameConverter))]
-        [Description("The name of the serial port used to communicate with the Pulse Pal.")]
+        [Description("The name of the serial port used to communicate with the Pulse Pal device.")]
         public string PortName
         {
-            get { return configuration.PortName; }
-            set { configuration.PortName = value; }
-        }
-
-        [Description("The collection of channel parameters used to configure the Pulse Pal.")]
-        public ChannelParameterCollection ChannelParameters
-        {
-            get { return configuration.ChannelParameters; }
+            get => configuration.PortName;
+            set => configuration.PortName = value;
         }
 
         /// <summary>
-        /// Generates an observable sequence that contains the serial connection object.
+        /// Gets the collection of channel parameters used to program the Pulse Pal.
+        /// </summary>
+        [Description("The collection of channel parameters used to program the Pulse Pal.")]
+        [Editor("Bonsai.Resources.Design.CollectionEditor, Bonsai.System.Design", DesignTypes.UITypeEditor)]
+        public ChannelParameterConfigurationCollection ChannelParameters => configuration.ChannelParameters;
+
+        /// <summary>
+        /// Generates an observable sequence that contains the created serial interface.
         /// </summary>
         /// <returns>
         /// A sequence containing a single instance of the <see cref="PulsePal"/> class
-        /// representing the serial connection.
+        /// representing the serial interface to the Pulse Pal device.
         /// </returns>
         public override IObservable<PulsePal> Generate()
         {
--- Bonsai.PulsePal/PulsePalConfiguration.cs
+++ Bonsai.PulsePal/PulsePalConfiguration.cs
@@ -1,22 +1,28 @@
 ﻿using System.ComponentModel;
-using Bonsai.IO;
+using Bonsai.IO.Ports;
+using Bonsai.PulsePal.Configuration;
 
 namespace Bonsai.PulsePal
 {
+    /// <summary>
+    /// Represents configuration settings used to program the operation of
+    /// a Pulse Pal device.
+    /// </summary>
     public class PulsePalConfiguration
     {
-        internal static readonly PulsePalConfiguration Default = new PulsePalConfiguration();
+        internal static readonly PulsePalConfiguration Default = new();
 
-        readonly ChannelParameterCollection channelParameters = new ChannelParameterCollection();
-
+        /// <summary>
+        /// Gets or sets the name of the serial port.
+        /// </summary>
         [Description("The name of the serial port.")]
         [TypeConverter(typeof(SerialPortNameConverter))]
         public string PortName { get; set; }
 
-        [Description("The collection of parameters used to specify operation of individual channels.")]
-        public ChannelParameterCollection ChannelParameters
-        {
-            get { return channelParameters; }
-        }
+        /// <summary>
+        /// Gets the collection of parameters used to program the operation of the Pulse Pal device.
+        /// </summary>
+        [Description("The collection of parameters used to program the operation of the Pulse Pal device.")]
+        public ChannelParameterConfigurationCollection ChannelParameters { get; } = new();
     }
 }
