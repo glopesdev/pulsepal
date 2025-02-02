47b0f33a27852bafaec70c9126072fe75712ca33
glopesdev <glopesdev@users.noreply.github.com>
2025-02-02 15:59:02 +0000

    Update docfx version and use separate member pages

--- docs/build.ps1
+++ docs/build.ps1
@@ -1,2 +1,2 @@
-.\bonsai\modules\Export-Image.ps1 "..\src\Bonsai.PulsePal\bin\Release\net462"
+.\bonsai\modules\Export-Image.ps1 "..\artifacts\bin\Bonsai.PulsePal\release_net462"
 dotnet docfx @args
